aa06da079573670d1ff8a3a60f85ef7618c37178
Andrew Lewis <andrewjlewis777@gmail.com>
2023-06-14 14:09:29 -0800

    practice

---
+++ Lab1c.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program
+{
+    public void Main()
+    {
+        string[] cars = {"Mustang", "Camero", "HellCat"};
+        Console.WriteLine(cars.Length);
+        Console.WriteLine(cars[2]);
+        
+    }
+}
