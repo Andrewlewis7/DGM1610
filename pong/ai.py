54fbede83e645972c364a51411d5f2ecca8bc9a1
Niels Turley <90103028+nielsturley@users.noreply.github.com>
2023-08-02 16:49:08 -0600

    ai stuff

--- pong/ai.py
+++ pong/ai.py
@@ -2,16 +2,19 @@ from paddle import Paddle
 from ball import Ball
 
 class AI:
-    def __init__(self, paddle, ball):
-        self.paddle = paddle(Paddle)
-        self.ball = ball(Ball)
+	def __init__(self, striker, ball):
+		self.striker = striker
+		self.ball = ball
+		self.height = striker.height
 
-    def move(self, ball):
-        if ball.posy < self.paddle.posy and self.paddle.posy > 0:
-            return -1
-        elif ball.posy > self.paddle.posy and self.paddle.posy < 400:
-            return 1
+	def move(self, ball):
+		if ball.posy < (self.striker.posy):
+			return -1
+		elif ball.posy > (self.striker.posy + self.height):
+			return 1
+		else:
+			return 0
 
-    def update(self):
-        YFac = self.move(self.ball)
-        self.paddle.update(YFac)
+	def update(self):
+		YFac = self.move(self.ball)
+		self.striker.update(YFac)
